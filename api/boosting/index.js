62fc04ba91f515138e9463921d1136bbc7d25378
bloxbyte <bloxbytestudio@gmail.com>
2026-04-27 23:53:17 +0100

    s

--- api/boosting/index.js
+++ api/boosting/index.js
@@ -11,8 +11,3 @@ app.post('/boosting', (req, res) => {
         res.status(400).send('Missing key parameter');
     }
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
