fe2e19d7fb6bc6066616c2133a24ad0096debf8d
bloxbyte <bloxbytestudio@gmail.com>
2026-05-04 18:31:24 +0100

    yes

--- scripts/assassin/main.lua
+++ scripts/assassin/main.lua
@@ -44,6 +44,3 @@ Objects["Settings"] = Objects["Window"]:CreateTab("Settings", "settings")
 loadstring(game:HttpGet("https://moonscripts.live/scripts/assassin/handlers/esp.lua"))().Init(Objects["MainTab"])
 loadstring(game:HttpGet("https://moonscripts.live/scripts/assassin/handlers/silentaim.lua"))().Init(Objects["MainTab"])
 --loadstring(game:HttpGet("https://moonscripts.live/scripts/assassin/handlers/autofarm.lua"))()
-
-print(ESP)
-print(SilentAim)
